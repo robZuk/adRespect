ce8e59d8c5de632cd38ec781877455d2d83cc554
Robert <robert.aleks.zuk@gmail.com>
2023-08-17 11:00:57 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import "./style.css";
+// import javascriptLogo from "./javascript.svg";
+// import viteLogo from "/vite.svg";
+// import { setupCounter } from "./counter.js";
+
+document.querySelector("#app").innerHTML = `
+  <div class="bg-red-100">
+    test
+   
+  </div>
+`;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./*.{html,js}",
+    "./pages/**/*.{html,js}",
+    "./components/**/*.{html,js}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
