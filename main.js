133a7c0fecec179178d243a2b84063d593631646
Robert <robert.aleks.zuk@gmail.com>
2023-08-20 14:55:46 +0200

    initial commit

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 import "./style.css";
-// import javascriptLogo from "./javascript.svg";
-// import viteLogo from "/vite.svg";
-// import { setupCounter } from "./counter.js";
 
-document.querySelector("#app").innerHTML = `
-  <div class="bg-red-100">
-    test
-   
-  </div>
-`;
+// navigation
+let showSearch = false;
+const searchInput = document.getElementById("search-input");
+const searchIcon = document.getElementById("search-icon");
+
+searchIcon.addEventListener("click", () => {
+  showSearch = !showSearch;
+
+  !showSearch && searchInput.classList.replace("w-48", "w-0");
+  !showSearch && searchInput.classList.replace("block", "hidden");
+  !showSearch && searchInput.classList.replace("px-2", "px-0");
+
+  showSearch && searchInput.classList.replace("w-0", "w-48");
+  showSearch && searchInput.classList.replace("hidden", "block");
+  showSearch && searchInput.classList.replace("px-0", "px-2");
+});
+
+// Initialization for ES Users
+import { Carousel, Dropdown, Ripple, initTE } from "tw-elements";
+
+initTE({ Carousel, Dropdown, Ripple });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,23 @@ export default {
     "./*.{html,js}",
     "./pages/**/*.{html,js}",
     "./components/**/*.{html,js}",
+    "./node_modules/tw-elements/dist/js/**/*.js",
   ],
   theme: {
+    fontFamily: {
+      Inter: ["Inter", "sans-serif"],
+      Montserat: ["Montserat", "sans-serif"],
+    },
+    colors: {
+      transparent: "transparent",
+      white: "white",
+      black: "black",
+      beige: "#DCC1AB",
+      green: "#1B5B31",
+      gray: "#F5F0EC",
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tw-elements/dist/plugin.cjs")],
+  darkMode: "class",
 };
